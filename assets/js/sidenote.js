c634e1ee14515f8fa3f05d0670a6290b835f0937
Edmund Lau <edmundlth95@gmail.com>
2021-11-22 20:50:55 +1100

    Fix javascript for handling sidenotes.

--- assets/js/sidenote.js
+++ assets/js/sidenote.js
@@ -4,10 +4,24 @@
 
 
 $(function() {
-    /* create checked checkbox */
-    var randomId = "snid" + Math.random().toString(16).slice(2);
-    $("span[class=sidenote]").before(
-        `<label for=\"${randomId}\" class=\"margin-toggle sidenote-number\"></label><input type=\"button\" class=\"margin-toggle\" id=\"${randomId}\"/>`
-        );
+    let _sidenoteCounter = 0
+    document.querySelectorAll("span.sidenote").forEach(
+        sidenote => {
+            let snLabel = document.createElement("label");
+            snLabel.setAttribute("for", `snid${_sidenoteCounter}`);
+            snLabel.setAttribute("class", "margin-toggle sidenote-number");
+            
+            let snInput = document.createElement("input");
+            snInput.setAttribute("type", "button");
+            snInput.setAttribute("class", "margin-toggle");
+            snInput.setAttribute("id", `snid${_sidenoteCounter}`);
+
+            sidenote.before(snLabel, snInput)
+            
+            // sidenote.before(
+            //     `<label for=\"snid${_sidenoteCounter}\" class=\"margin-toggle sidenote-number\"></label><input type=\"button\" class=\"margin-toggle\" id=\"snid${_sidenoteCounter}\"/>`
+            // );
+            _sidenoteCounter = _sidenoteCounter + 1;
+	});
   });
   
